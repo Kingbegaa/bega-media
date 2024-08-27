622a3728c33a265775a1d1a86358e984af02dba1
Kingbegaa <barakachachabegaa@gmail.com>
2024-08-27 21:17:48 +0300

    Update repo.js

--- bega/repo.js
+++ bega/repo.js
@@ -37,8 +37,8 @@ let infoMsg =  `
 ( ͡° ͜ʖ ͡°) 𝐎𝐳𝐚𝐚𝐡  👊 𝐭𝐡𝐢𝐬 𝐢𝐬  𝐛𝐚𝐫𝐚𝐤𝐚_𝐦𝐝   𝐜𝐫𝐞𝐚𝐭𝐞𝐝  𝐛𝐲  𝐛𝐚𝐫𝐚𝐤𝐚_𝐛𝐞𝐠𝐚
 ( ͡° ͜ʖ ͡°) 𝐏𝐥𝐞𝐚𝐬𝐞 𝐠𝐢𝐯𝐞 𝐚 𝐬𝐭𝐚𝐫  🌟 𝐨𝐧 𝐭𝐡𝐞 𝐫𝐞𝐩𝐨 𝐚𝐧𝐝 𝐟𝐨𝐫𝐤 𝐢𝐭  🚗  𝐤𝐢𝐧𝐝𝐥𝐲
 
-( ͡° ͜ʖ ͡°) 𝐑𝐞𝐩𝐨 [https://github.com/Kingbega/BARAKA-MD-V1]  
-( ͡° ͜ʖ ͡°) 𝐑𝐞𝐩𝐨 [https://github.com/Kingbega/BARAKA-MD-V1]
+( ͡° ͜ʖ ͡°) 𝐑𝐞𝐩𝐨 [https://github.com/Kingbegaa/BARAKA-MD-V2]  
+( ͡° ͜ʖ ͡°) 𝐑𝐞𝐩𝐨 [https://github.com/Kingbegaa/BARAKA-MD-V2]
   `;
     
 let menuMsg = `
