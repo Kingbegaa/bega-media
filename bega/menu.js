f78ad6518f299e37e702d307b0ed34195a08173c
Kingbegaa <barakachachabegaa@gmail.com>
2024-09-13 21:06:33 +0300

    Update menu.js

--- bega/menu.js
+++ bega/menu.js
@@ -1,101 +1,84 @@
-const util = require('util');
-const fs = require('fs-extra');
-const { zokou } = require(__dirname + "/../framework/zokou");
-const { format } = require(__dirname + "/../framework/mesfonctions");
-const os = require("os");
+const {
+  zokou
+} = require(__dirname + "/../framework/zokou");
+const {
+  format
+} = require(__dirname + "/../framework/mesfonctions");
+const os = require('os');
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-
-zokou({ nomCom: "menu", reaction:"📁",categorie: "Général" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLowerCase() != "oui") {
-        mode = "privé";
+zokou({
+  'nomCom': "menu",
+  'categorie': "General"
+}, async (_0x242c41, _0x7e045, _0x2548fa) => {
+  let {
+    ms: _0x3136fb,
+    repondre: _0x247b6d,
+    prefixe: _0x3ede80,
+    nomAuteurMessage: _0x38a131,
+    mybotpic: _0x5164fc
+  } = _0x2548fa;
+  let {
+    cm: _0x1eb586
+  } = require(__dirname + "/../framework//zokou");
+  var _0x443d1b = {};
+  var _0x3d00f2 = "public";
+  if (s.MODE.toLocaleLowerCase() != "yes") {
+    _0x3d00f2 = "private";
+  }
+  _0x1eb586.map(async (_0x359147, _0x283436) => {
+    if (!_0x443d1b[_0x359147.categorie]) {
+      _0x443d1b[_0x359147.categorie] = [];
     }
-
-     
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
-    moment.tz.setDefault('Etc/GMT');
-
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-  let infoMsg =  `
-╭────✧${s.BOT}✧────◆
-│   *Préfixe* : ${s.PREFIXE}
-│   *Owner* : ${s.NOM_OWNER}
-│   *Mode* : ${mode}
-│   *Commandes* : ${cm.length}
-│   *Date* : ${date}
-│   *Heure* : ${temps}
-│   *Mémoire* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-│   *Plateforme* : ${os.platform()}
-│   *Développeurs* : Baraka
-╰─────✧WA-BOT✧─────◆ \n\n`;
-    
-let menuMsg = `
-👋 salut ${nomAuteurMessage} 👋
-
-*Voici la liste de mes commandes :*
-◇                             ◇
-`;
-
-    for (const cat in coms) {
-        menuMsg += `╭────❏ *${cat}* ❏`;
-        for (const cmd of coms[cat]) {
-            menuMsg += `
-│ ${cmd}`;
-        }
-        menuMsg += `
-╰═════════════⊷ \n`
+    _0x443d1b[_0x359147.categorie].push(_0x359147.nomCom);
+  });
+  moment.tz.setDefault("EAT");
+  const _0x496af2 = moment().format("HH:mm:ss");
+  let _0x10a699 = "\n╭──────𝙱𝙰𝚁𝙰𝙺𝙰-𝙼𝙳──────❂\n┊❂╭─────────────────❂\n┊✺┊ *User* : " + s.OWNER_NAME + "\n┊✺┊ *Mode* : " + _0x3d00f2 + "\n┊✺┊ *Commands* : 240\n┊✺┊ *Time* : " + _0x496af2 + " \n┊✺┊ *Ram* : " + format(os.totalmem() - os.freemem()) + '/' + format(os.totalmem()) + "\n┊❂╰─────────────────❂\n╰───────────────────❂ \n\n";
+  let _0x2a221b = " \n*╭════════════════════════❂*\n";
+  for (const _0x553f3d in _0x443d1b) {
+    _0x2a221b += "*╭────❂* *" + _0x553f3d + "* *❂*";
+    for (const _0x3ac13d of _0x443d1b[_0x553f3d]) {
+      _0x2a221b += " \n*┊* " + _0x3ac13d;
     }
-
-    menuMsg += `
-◇            ◇
-*»»————— ★ —————««*
-Pour utiliser  une  commande, tapez  ${prefixe}"nom de la commande"
- 
- powered by Baraka-Md 
-                                                
-*»»————— ★ —————««*
-`;
-
-    
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
+    _0x2a221b += "\n*╰═════════════❂* \n";
+  }
+  _0x2a221b += "\n \n *𝙼𝙰𝙳𝙴 𝙱𝚈 𝙱𝙰𝚁𝙰𝙺𝙰 𝙲𝙷𝙰𝙲𝙷𝙰 𝙱𝙴𝙶𝙰*\n*╰═════════════════════════❂*\n";
+  var _0x2d51c8 = _0x5164fc();
+  if (_0x2d51c8.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true}, { quoted: ms });
+      _0x7e045.sendMessage(_0x242c41, {
+        'video': {
+          'url': _0x2d51c8
+        },
+        'caption': _0x10a699 + _0x2a221b,
+        'footer': "Je suis *Baraka-MD*, développé par Djalega++",
+        'gifPlayback': true
+      }, {
+        'quoted': _0x3136fb
+      });
+    } catch (_0x57059d) {
+      console.log("🥵🥵 Menu erreur " + _0x57059d);
+      _0x247b6d("🥵🥵 Menu erreur " + _0x57059d);
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+  } else {
+    if (_0x2d51c8.match(/\.(jpeg|png|jpg)$/i)) {
+      try {
+        _0x7e045.sendMessage(_0x242c41, {
+          'image': {
+            'url': _0x2d51c8
+          },
+          'caption': _0x10a699 + _0x2a221b,
+          'footer': "*Baraka-Tech*"
+        }, {
+          'quoted': _0x3136fb
+        });
+      } catch (_0x2a786d) {
+        console.log("🥵🥵 Menu erreur " + _0x2a786d);
+        _0x247b6d("🥵🥵 Menu erreur " + _0x2a786d);
+      }
+    } else {
+      _0x247b6d(_0x10a699 + _0x2a221b);
     }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-    try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" }, { quoted: ms });
-    }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
-
+  }
 });
